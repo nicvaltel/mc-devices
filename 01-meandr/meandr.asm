3ed0925d0b6585580229e9d8d76707c8b16fd6bc
nicvaltel <nicvaltel@gmail.com>
2024-10-19 09:19:55 +0300

    Simple meandr oscilator with 8 harmonics

---
+++ 01-meandr/meandr.asm
@@ -0,0 +1,22 @@
+;---- Определяем целевое устройство
+	;; .device atmega8
+	.include "m8def.inc"
+
+;---- Сегмент данных
+        .dseg
+;---- Сегмент кода
+        .cseg
+        .org 0x0000
+
+	clr r16
+	ldi r17,0xff
+	out ddrb, r17
+	
+
+loop:
+	out PORTB, r17
+	inc r17
+	rjmp loop
+
+;---- Сегмент EEPROM
+        .eseg
