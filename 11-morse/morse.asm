07b5d5dcf4f5eb8d348bc9b1a63eb0129035141c
nicvaltel <nicvaltel@gmail.com>
2024-11-06 20:19:38 +0300

    Morse v0.2 - sound 1 litera

--- 11-morse/morse.asm
+++ 11-morse/morse.asm
@@ -2,7 +2,9 @@
 .include "m16def.inc"
 
 .equ TIMER_COUNTER = 0x101
-.equ SOUND_SIGNAL = 0x102
+.equ WAVE_COUNTER = 0x102
+; .equ SOUND_SIGNAL = 0x103
+.equ CURRENT_LITERA_CODE_OFFSET = 0x103
 .equ CURRENT_LITERA_CODE = 0x110
 .equ ALPHABET = 0x130
 .equ MESSAGE = 0x170
@@ -26,18 +28,28 @@ RESET:
     rcall INIT_ALPHABET
     rcall INIT_MESSAGE
 
+    ; set CURRENT_LITERA_CODE_OFFSET = 0
+    clr r16
+    ldi r27, high(CURRENT_LITERA_CODE_OFFSET)
+    ldi r27, low(CURRENT_LITERA_CODE_OFFSET)
+    st X, r16
+
+    ; load litera 6
     ldi r27, high(MESSAGE)
     ldi r26, low(MESSAGE)
     inc r26
     inc r26
     inc r26
-    rjmp LOAD_LITERA
+    rcall LOAD_LITERA
 
-    ; set SOUND_SIGNAL = 0
-    ldi r27, high(SOUND_SIGNAL) ; X = R27:R26
-    ldi r26, low(SOUND_SIGNAL)
-    clr r16
-    st X,r16
+    ; ; set SOUND_SIGNAL_FLAG = 1
+    ldi r23, 0xff ; r23 is global variable = sound signal flag (is sound = 0xff or pause =0)
+
+    ; ; set SOUND_SIGNAL = 0
+    ; ldi r27, high(SOUND_SIGNAL) ; X = R27:R26
+    ; ldi r26, low(SOUND_SIGNAL)
+    ; clr r16
+    ; st X,r16
 
     ; set TIMER_COUNTER = 0
     ldi r27, high(TIMER_COUNTER) ; X = R27:R26
@@ -73,6 +85,7 @@ MAIN:
 ; Interrupt Service Routine for Timer0 overflow
 TIM0_OVF_ISR:
     push r16
+    ; push r17
     push r26
     push r27
     in r16, SREG
@@ -96,21 +109,77 @@ TIM0_OVF_ISR:
     st X, r16 
 
     ; load SOUND_SIGNAL
-    ldi r27, high(SOUND_SIGNAL) ; X = R27:R26
-    ldi r26, low(SOUND_SIGNAL)
-    ld r16, X
+    in r16, porta
 
     ; save inverted SOUND_SIGNAL
     com r16
+    and r16, r23 ; r23 - global var = sound signal flag
+    out porta, r16
+
+    ; load WAVE_COUNTER
+    ldi r27, high(WAVE_COUNTER)
+    ldi r26, low(WAVE_COUNTER)
+    ld r16, X
+
+    ; increment wave counter
+    inc r16
     st X, r16
 
-    out porta, r16
+    cpi r16, 44 ;; check if max wave counter = 44 ; 44/220Hz = 0.2 sec of length of 1 dot or pause
+    brne TIM0_OVF_ISR_FINISH ; if wave counter /= 44 - goto finish
+    ; else clear vawe counter and load next litera code
+
+    ; clear wave counter
+    clr r16
+    st X, r16
+
+    ; load CURRENT_LITERA_CODE_OFFSET
+    ldi r27, high(CURRENT_LITERA_CODE_OFFSET)
+    ldi r26, low(CURRENT_LITERA_CODE_OFFSET)
+    ld r16, X
+
+    ; increment CURRENT_LITERA_CODE_OFFSET
+    inc r16
+    st X, r16
+
+    ; load CURRENT_LITERA_CODE
+    ldi r27, high(CURRENT_LITERA_CODE) ; X = R27:R26
+    ldi r26, low(CURRENT_LITERA_CODE)
+    add r26, r16 ; add CURRENT_LITERA_CODE offset
+    ld r16, X
+
+    ; check for end of litera
+    cpi r16, 0xff
+    breq TIM0_OVF_SIGNAL_FLAG_IS_0xff
+
+    ; check for litera bit sound code
+    cpi r16, 1
+    breq TIM0_OVF_SIGNAL_FLAG_IS_1
+
+    ; TIM0_OVF_SIGNAL_FLAG_IS_0:
+    ldi r23, 0
+    out porta, r23
+    rjmp TIM0_OVF_ISR_FINISH
+
+    TIM0_OVF_SIGNAL_FLAG_IS_1:
+    ldi r23, 0xff
+    out porta, r23
+    rjmp TIM0_OVF_ISR_FINISH
+
+    TIM0_OVF_SIGNAL_FLAG_IS_0xff:
+    ; set CURRENT_LITERA_CODE_OFFSET = 0
+    clr r16
+    ldi r27, high(CURRENT_LITERA_CODE_OFFSET)
+    ldi r26, low(CURRENT_LITERA_CODE_OFFSET)
+    st X, r16
+
 
     TIM0_OVF_ISR_FINISH:
     pop r16
     out SREG, r16
     pop r27
     pop r26
+    ; pop r17
     pop r16
 
     reti
@@ -149,8 +218,7 @@ LOAD_LITERA:
 
     LOAD_LITERA_LOOP:
     ; if litera length = 0 jump to finish
-    clr r16
-    cp r20, r16
+    cpi r20, 0
     breq FINISH_LOAD_LITERA
 
     clr r17
@@ -198,6 +266,10 @@ LOAD_LITERA:
 
     FINISH_LOAD_LITERA_POP_LOOP:
 
+    clr r17
+    st X, r17 ; add second pause after end of litera (third pause will be 0xff)
+    inc r26
+    
     ser r17
     st X, r17 ; set to end of CURRENT_LITERA_CODE value 0xff
 
